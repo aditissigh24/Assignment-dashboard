825db57586c85ba4a7279641d9450d1bc3ee9f79
aditissigh24 <aditissigh24@gmail.com>
2024-12-20 14:01:01 +0530

    deployed on bvercel

--- src/app/Components/Users.tsx
+++ src/app/Components/Users.tsx
@@ -83,7 +83,7 @@ export default function Users() {
       <div>
         <MenuButton className="inline-flex w-full justify-center gap-x-1 rounded-md bg-white hover:bg-blue-100 px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300">
           
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8">
             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
           </svg>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,25 @@
 'use client';
 import User from "./Components/UserProfile";
 import UserAcitivityGrid from "./Components/UserAcitivityGrid";
-import Users from "./Components/Users";
-import UserDetails from "./Components/UserDetails";
+const Users = dynamic(() => import('./Components/Users'), { ssr: false });
+const UserDetails = dynamic(() => import('./Components/UserDetails'), { ssr: false });
+import { useEffect, useState } from "react";
+import dynamic from 'next/dynamic';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // This will ensure that we are only rendering on the client
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Render nothing during SSR
+  }
+
+  
   return (
     <Router>
       <div className="flex justify-between lg:justify-around  mt-6 p-2">
